93a7b6b6afd88732ab65899cce637372280a510f
omkar-pd <omkarpd109@gmail.com>
2023-01-05 22:00:05 +0530

    added account route

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -26,7 +26,7 @@ export const Main = () => {
           src={`${baseUrl}${movie?.backdrop_path}`}
           alt={movie?.title}
         />
-        <div className="absolute w-full  top-[20%] p-4 md:p-8">
+        <div className="absolute w-full top-[20%] p-4 md:p-8">
           <h1 className="text-3xl md:text-5xl font-bold">{movie?.title}</h1>
           <div className="my-4">
             <button className="border bg-gray-300 text-black border-gray-300 py-2 px-5">
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -5,6 +5,7 @@ export const Login = () => {
   const navigate = useNavigate();
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
   const { user, logIn } = UserAuth();
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -13,6 +14,7 @@ export const Login = () => {
       navigate("/");
     } catch (error) {
       console.log(error);
+      setError(error.message);
     }
   };
   return (
@@ -27,6 +29,7 @@ export const Login = () => {
         <div className="max-w-[450px] h-[600px] mx-auto bg-black/75 text-white">
           <div className="max-w-[320px] mx-auto py-16">
             <h1 className="text-3xl font-bold">Sign In</h1>
+            {error ? <p className="p-3 bg-red-400 my-2 "> {error}</p> : null}
             <form onSubmit={handleSubmit} className="w-full flex flex-col py-4">
               <input
                 className="p-3 my-2 bg-gray-500 rounded"
